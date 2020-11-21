8f1f7a88cfbb3d8f6cdc80ee280ace32ebe83c72
Duy Quang <61661668+duyquang19981@users.noreply.github.com>
2020-11-21 17:48:26 +0700

    Updates

---
+++ precache-manifest.e81f14f76b11e335109d54a79890eee7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1744f37d90c801ee8d287892fd85d129",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "843f8c94ec5757712483",
+    "url": "/monsters-rolodex/static/css/main.cb07f1de.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "843f8c94ec5757712483",
+    "url": "/monsters-rolodex/static/js/main.f3ef49a4.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
